29001f293d390f2427b84b2d2a7ab62312464d45
ItsAvi003 <iandiehard@gmail.com>
2026-03-24 16:25:12 +0530

    loggings and common functions are updated

---
+++ src/cnnClassifier/utils/common.py
@@ -0,0 +1,47 @@
+import os
+from box.exceptions import BoxValueError
+import yaml
+from cnnClassifier import logger
+import json
+import joblib
+from ensure import ensure_annotations
+from box import ConfigBox
+from pathlib import Path
+from typing import Any
+import base64
+
+@ensure_annotations
+def read_yaml(path_to_yaml: Path)->ConfigBox:
+    try:
+        with open(path_to_yaml) as yaml_file:
+            content=yaml.safe_load(yaml_file)
+            logger.info(f"yaml file: {path_to_yaml} is successfully loaded")
+            return ConfigBox(content)
+    except BoxValueError:
+        raise ValueError("yaml file is empty")
+    except Exception as e:
+        raise e 
+
+@ensure_annotations
+def load_json(path: Path)->ConfigBox:
+    with open(path) as f:
+        content=json.load(f)
+    logger.info(f"Json file : {path} is succesfully loaded")
+    return ConfigBox(content)  
+    
+@ensure_annotations
+def get_size(path: Path) -> str:
+    size_in_kb = round(os.path.getsize(path)/1024)
+    return f"~ {size_in_kb} KB"
+
+
+def decodeImage(imgstring, fileName):
+    imgdata = base64.b64decode(imgstring)
+    with open(fileName, 'wb') as f:
+        f.write(imgdata)
+        f.close()
+
+
+def encodeImageIntoBase64(croppedImagePath):
+    with open(croppedImagePath, "rb") as f:
+        return base64.b64encode(f.read())
